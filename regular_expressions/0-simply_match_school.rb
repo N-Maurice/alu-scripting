82d05784647bdce6a4f4f367e858aa388481e0c3
N-Maurice <m.nshimyumu@alustudent.com>
2025-05-10 14:51:54 -0700

    adding more blocks of code

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,9 +1,2 @@
 #!/usr/bin/env ruby
-input = ARGV[0]
-
-# Define a regex that matches exactly "School"
-if input =~ /School/
-  puts "Match found"
-else
-  puts "No match"
-end
+puts ARGV[0].scan(/school/).join
